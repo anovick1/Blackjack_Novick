4b9a408bc252debcb31969e5b6a19771a97b11e0
Avery Novick <averynov@yahoo.com>
2022-07-04 15:10:38 -0400

    hit button event listener

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ let dealer = []
 let user = []
 let userSum = 0
 let dealerSum = 0
+let count = 0
 
 let buttons = document.querySelector('.buttons')
 let stats = document.querySelector('.stats')
 let statLine = document.createElement('h1')
 let startBtn = document.createElement('button')
+let hitBtn = document.createElement('button')
+let standBtn = document.createElement('button')
 startBtn.innerText = 'Play Game'
+hitBtn.innerText = 'Hit'
+standBtn.innerText = 'Stand'
 buttons.append(startBtn)
 
 let d0 = document.querySelector('#d0')
@@ -75,6 +80,8 @@ const dealCard = () => {
 }
 
 const sum = () => {
+  dealerSum = 0
+  userSum = 0
   for (let i = 0; i < dealer.length; i++) {
     dealerSum += dealer[i].value
   }
@@ -83,6 +90,24 @@ const sum = () => {
   }
   statLine.innerText = ' Dealer Sum: ' + dealerSum + '\n\nYour Sum: ' + userSum
   stats.append(statLine)
+}
+
+const dealerHit = () => {
+  if (dealerSum < 17) {
+    if (dealer.length === 2) {
+      dealer.push(dealCard())
+      dealerCard2.innerText = dealer[2].value
+      d2.append(dealerCard2)
+    } else if (dealer.length === 3) {
+      dealer.push(dealCard())
+      dealerCard3.innerText = dealer[3].value
+      d3.append(dealerCard3)
+    } else if (dealer.length === 4) {
+      dealer.push(dealCard())
+      dealerCard4.innerText = dealer[4].value
+      d4.append(dealerCard4)
+    }
+  }
 }
 
 startBtn.addEventListener('click', () => {
@@ -103,4 +128,30 @@ startBtn.addEventListener('click', () => {
   d1.append(dealerCard1)
   sum()
   startBtn.style.display = 'none'
+  buttons.append(hitBtn)
+  buttons.append(standBtn)
+})
+
+hitBtn.addEventListener('click', () => {
+  if (user.length === 2) {
+    user.push(dealCard())
+    userCard2.innerText = user[2].value
+    u2.append(userCard2)
+  } else if (user.length === 3) {
+    user.push(dealCard())
+    userCard3.innerText = user[3].value
+    u3.append(userCard3)
+  } else if (user.length === 4) {
+    user.push(dealCard())
+    userCard4.innerText = user[4].value
+    u4.append(userCard4)
+  }
+  dealerHit()
+  sum()
+})
+
+standBtn.addEventListener('click', () => {
+  while (dealerSum < 17) {
+    dealerHit()
+  }
 })

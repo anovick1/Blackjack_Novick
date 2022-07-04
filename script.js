95a9b6d6ab4266574b92d6eaae9ee30d1d2791c8
Avery Novick <averynov@yahoo.com>
2022-07-04 13:51:46 -0400

    display value of dealt cards

--- script.js
+++ script.js
@@ -3,16 +3,30 @@ let dealer = []
 let user = []
 let userSum = 0
 let dealerSum = 0
-let c0 = document.querySelector('c0')
-let c1 = document.querySelector('c1')
-let c2 = document.querySelector('c2')
-let c3 = document.querySelector('c3')
-let c4 = document.querySelector('c4')
-let c5 = document.querySelector('c5')
-let c6 = document.querySelector('c6')
-let c7 = document.querySelector('c7')
-let c8 = document.querySelector('c8')
-let c9 = document.querySelector('c9')
+
+let buttons = document.querySelector('buttons')
+let stats = document.querySelector('.stats')
+
+let d0 = document.querySelector('#d0')
+let d1 = document.querySelector('#d1')
+let d2 = document.querySelector('#d2')
+let d3 = document.querySelector('#d3')
+let d4 = document.querySelector('#d4')
+let u0 = document.querySelector('#u0')
+let u1 = document.querySelector('#u1')
+let u2 = document.querySelector('#u2')
+let u3 = document.querySelector('#u3')
+let u4 = document.querySelector('#u4')
+let dealerCard0 = document.createElement('h1')
+let dealerCard1 = document.createElement('h1')
+let dealerCard2 = document.createElement('h1')
+let dealerCard3 = document.createElement('h1')
+let dealerCard4 = document.createElement('h1')
+let userCard0 = document.createElement('h1')
+let userCard1 = document.createElement('h1')
+let userCard2 = document.createElement('h1')
+let userCard3 = document.createElement('h1')
+let userCard4 = document.createElement('h1')
 
 /// make card objects
 // 0 = hearts
@@ -58,9 +72,20 @@ const dealCard = () => {
 
 const start = () => {
   user.push(dealCard())
+  userCard0.innerText = user[0].value
+  u0.append(userCard0)
+
   dealer.push(dealCard())
+  dealerCard0.innerText = dealer[0].value
+  d0.append(dealerCard0)
+
   user.push(dealCard())
+  userCard1.innerText = user[1].value
+  u1.append(userCard1)
+
   dealer.push(dealCard())
+  dealerCard1.innerText = dealer[1].value
+  d1.append(dealerCard1)
 }
 start()
 
@@ -74,5 +99,3 @@ const sum = () => {
 }
 
 sum()
-console.log(userSum)
-console.log(dealerSum)
